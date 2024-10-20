ec47cc9f493f80ffb2f1cf607c1c32e73ce77a10
jhjeon <jhjeon3040@gmail.com>
2024-10-20 12:26:08 +0900

    [fix] fix

--- handler/chat.py
+++ handler/chat.py
@@ -1,8 +1,8 @@
 import asyncio
 import json
+from contextlib import asynccontextmanager
 from typing import List
 
-from aiokafka import AIOKafkaProducer
 from aioredis import Redis
 from fastapi import APIRouter, Depends
 from sqlalchemy import delete, select
@@ -13,7 +13,6 @@ from starlette.websockets import WebSocket, WebSocketDisconnect
 from auth import get_current_user
 from common.redis_conf import get_redis
 from database import get_db
-from handler.chat_kafka import get_kafka_consumer, get_kafka_producer
 from model.chat import Chat, ChatUser
 from model.user import User
 from schema.chat import ChatResponse
@@ -22,49 +21,11 @@ from schema.user import UserResponse
 router = APIRouter(tags=["Chat"])
 
 
-@router.websocket("/chat/ws")
-async def websocket_endpoint(
-        room_id: str,
-        jwt_token: str,
-        websocket: WebSocket,
-        redis: Redis = Depends(get_redis),
-):
-    current_user = await get_current_user(jwt_token)
-    await websocket.accept()
-
-    pubsub = redis.pubsub()
-    await pubsub.subscribe(room_id)
-
-    async def listen_to_redis():
-        while True:
-            message = await pubsub.get_message(ignore_subscribe_messages=True)
-            if message:
-                await websocket.send_text(message["data"])
-
-    listen_task = asyncio.create_task(listen_to_redis())
+@asynccontextmanager
+async def redis_listener(pubsub, websocket: WebSocket):
+    listen_task = asyncio.create_task(listen_to_redis(pubsub, websocket))
     try:
-        while True:
-            msg = await websocket.receive_text()
-            data = json.dumps(
-                {
-                    "user": current_user.name,
-                    "message": msg,
-                },
-                ensure_ascii=False,
-            )
-
-            await redis.rpush(room_id, data)
-            await redis.publish(room_id, data)
-    except WebSocketDisconnect:
-        try:
-            await redis.publish(
-                f"room:{room_id}",
-                json.dumps(
-                    {"user": current_user.name, "message": "has disconnected."}
-                ),
-            )
-        except WebSocketDisconnect:
-            pass
+        yield listen_task
     finally:
         listen_task.cancel()
         try:
@@ -73,69 +34,45 @@ async def websocket_endpoint(
             print("Redis listener task cancelled successfully.")
 
 
-# @router.websocket("/chat/ws/kafka")
+async def listen_to_redis(pubsub, websocket: WebSocket):
+    while True:
+        message = await pubsub.get_message(ignore_subscribe_messages=True)
+        if message:
+            await websocket.send_text(
+                message["data"].decode("utf-8")
+            )  # 메시지 전송
+
+
+@router.websocket("/chat/ws")
 async def websocket_endpoint(
-        room_id: str,
-        jwt_token: str,
-        websocket: WebSocket,
-        kafka_producer: AIOKafkaProducer = Depends(get_kafka_producer),
+    room_id: str,
+    jwt_token: str,
+    websocket: WebSocket,
+    redis: Redis = Depends(get_redis),
 ):
     current_user = await get_current_user(jwt_token)
     await websocket.accept()
 
-    # Kafka 컨슈머 생성 및 시작
-    kafka_consumer = await get_kafka_consumer(room_id)
+    pubsub = redis.pubsub()
+    await pubsub.subscribe(room_id)
 
-    async def listen_to_kafka():
-        async for message in kafka_consumer:
-            # Kafka에서 수신한 메시지를 WebSocket 클라이언트로 전송
-            msg_data = message.value.decode("utf-8")
-            await websocket.send_text(msg_data)
-
-    # Kafka 메시지 구독을 위한 비동기 Task 생성
-    listen_task = asyncio.create_task(listen_to_kafka())
-
-    try:
-        # WebSocket 메시지 처리 루프
-        while True:
-            msg = await websocket.receive_text()
-            data = json.dumps(
-                {
-                    "user": current_user.name,
-                    "message": msg,
-                },
-                ensure_ascii=False,
-            )
-
-            # Kafka에 메시지를 발행 (Produce)
-            await kafka_producer.send_and_wait(room_id, data.encode("utf-8"))
-
-    except WebSocketDisconnect:
+    async with redis_listener(pubsub, websocket):
         try:
-            # 연결 해제 메시지를 Kafka에 발행
-            disconnect_msg = json.dumps(
-                {"user": current_user.name, "message": "has disconnected."},
-                ensure_ascii=False,
-            )
-            await kafka_producer.send_and_wait(
-                room_id, disconnect_msg.encode("utf-8")
-            )
+            while True:
+                text = await websocket.receive_text()
+                msg = {"user": current_user.name, "message": text}
+                data = json.dumps(msg, ensure_ascii=False)
+                await redis.rpush(room_id, data)  # Redis에 메시지 저장
+                await redis.publish(room_id, data)  # Redis에 메시지 발행
         except WebSocketDisconnect:
             pass
-    finally:
-        # Kafka 컨슈머와 프로듀서 정리
-        listen_task.cancel()
-        try:
-            await listen_task
-        except asyncio.CancelledError:
-            print("Kafka listener task cancelled successfully.")
-        await kafka_consumer.stop()
-        await kafka_producer.stop()
+        finally:
+            await pubsub.unsubscribe(room_id)
 
 
 @router.get("/chat/history")
 async def get_history(
-        room_id: str, offset: int = 0, limit: int = 20, redis=Depends(get_redis)
+    room_id: str, offset: int = 0, limit: int = 20, redis=Depends(get_redis)
 ):
     history = await redis.lrange(room_id, offset, offset + limit - 1)
     return {"history": [message for message in history]}
@@ -143,9 +80,9 @@ async def get_history(
 
 @router.post("/chat", response_model=ChatResponse)
 async def create_chat(
-        to_user_id: str,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user),
+    to_user_id: str,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
 ):
     async with db.begin():
         chat = Chat()
@@ -168,8 +105,8 @@ async def create_chat(
 
 @router.get("/chat", response_model=List[ChatResponse])
 async def get_chat(
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
 ):
     result = await db.execute(
         select(Chat).join(Chat.users).where(User.id == current_user.id)
@@ -187,9 +124,9 @@ async def get_chat(
 
 @router.delete("/chat/{chat_id}")
 async def delete_chat(
-        chat_id: str,
-        db: AsyncSession = Depends(get_db),
-        current_user: User = Depends(get_current_user),
+    chat_id: str,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
 ):
     result = await db.execute(delete(Chat).where(Chat.id == chat_id))
     if result.rowcount == 0:
